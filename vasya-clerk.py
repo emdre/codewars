77874c91eca24c4de8d5b26243d5a77228922387
emdree <magdalena.drej@gmail.com>
2019-06-20 18:28:27 +0200

    new task

---
+++ vasya-clerk.py
@@ -0,0 +1,35 @@
+"""
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+
+Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
+
+Examples:
+tickets([25, 25, 50]) # => YES 
+tickets([25, 100]) # => NO. Vasya will not have enough money to give change to 100 dollars
+tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+"""
+
+def tickets(people):
+    change25 = 0
+    change50 = 0
+    change100 = 0
+    for bill in people:
+        if bill == 25:
+            change25 += 1
+        elif bill == 50:
+            change50 += 1
+            change25 -= 1
+        elif bill == 100:
+            change100 += 1
+            if change50 >= 1:
+                change50 -= 1
+                change25 -= 1
+            else:
+                change25 -= 3
+        if change25 + change50 + change100 < 0 or change25 < 0:
+            return "NO"
+    return "YES"
